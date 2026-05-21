241a1e49b2c3aa2aaaecbff713bcf39e7f0673de
theforeveriris <theforeveriris@proton.me>
2026-05-21 04:59:49 +0000

    config: 更新 Giscus 配置

--- js/config.js
+++ js/config.js
@@ -2,22 +2,22 @@
   window.MarkdownPreview = window.MarkdownPreview || {};
   
   window.MarkdownPreview.CONFIG = {
-    owner: 'theforeveriris',
-    repo: 'md-preview',
-    giscus: {
-      enabled: true,
-      repo: 'theforeveriris/md-preview',
-      repoId: '',
-      category: 'Announcements',
-      categoryId: '',
-      mapping: 'pathname',
-      strict: '0',
-      reactionsEnabled: '1',
-      emitMetadata: '0',
-      inputPosition: 'bottom',
-      theme: 'light',
-      lang: 'zh-CN',
-      loading: 'lazy'
-    }
-  };
+  owner: 'theforeveriris',
+  repo: 'md-preview',
+  giscus: {
+    enabled: true,
+    repo: 'theforeveriris/md-preview',
+    repoId: 'R_kgDOShdDkQ',
+    category: 'Announcements',
+    categoryId: 'DIC_kwDOShdDkc4C9g0r',
+    mapping: 'pathname',
+    strict: '0',
+    reactionsEnabled: '1',
+    emitMetadata: '0',
+    inputPosition: 'bottom',
+    theme: 'preferred_color_scheme',
+    lang: 'zh-CN',
+    loading: 'lazy'
+  }
+};
 })();
